fd2f3b19bd7075b2debcbda9224e5daf1d5e4d69
UstinskiyDmitriy <ustinskiy.dmitriy1997@yandex.ru>
2024-11-11 19:03:21 +0300

    some changes

--- src/components/game-card/GameCard.tsx
+++ src/components/game-card/GameCard.tsx
@@ -1,7 +1,10 @@
-import s from './GameCard.module.css'
-import LikeButton from '../../ui/like-button/LikeButton';
-import { Link } from 'react-router-dom';
-import CartButton from '../../ui/cart-button/CartButton';
+import s from "./GameCard.module.css";
+import LikeButton from "../../ui/like-button/LikeButton";
+import { Link } from "react-router-dom";
+import CartButton from "../../ui/cart-button/CartButton";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../services/store/store";
+import { addToCart } from "../../services/slices/cartSlice";
 
 interface TCard {
   id: number;
@@ -9,45 +12,73 @@ interface TCard {
   title?: string;
   isonline?: string;
   site?: string;
-  
 }
 
 interface TGameCard {
   id: number;
-  image: string;
+  image: string | undefined;
   title: string;
   price: number | string | undefined;
   isonline?: string;
   card: TCard;
-  onClick: () => void
+  onClick: () => void;
 }
 
-export default function GameCard({id, image, title, price, onClick, card}: TGameCard) {
-  const handleOverlayClick = (e: React.MouseEvent) => {
+export default function GameCard({
+  id,
+  image,
+  title,
+  price,
+  onClick,
+  card,
+}: TGameCard) {
+  const dispatch = useDispatch();
+  const isInCart = useSelector((state: RootState) =>
+    state.cart.cart.some((item) => item.id === id)
+  );
+
+  const handleAddToCart = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.preventDefault();
     e.stopPropagation();
+    if (!isInCart) {
+      dispatch(
+        addToCart({
+          id,
+          image,
+          title,
+          price,
+        })
+      );
+    }
   };
 
   return (
     <div className={s.main} onClick={onClick}>
-      <Link to={`about/${card.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-        <div className={s.image_wrapper} >
-          <img src={image} alt={title} className={s.image}/>
+      <Link
+        to={`about/${card.id}`}
+        style={{ textDecoration: "none", color: "inherit" }}
+      >
+        <div className={s.image_wrapper}>
+          <img src={image} alt={title} className={s.image} />
         </div>
-        </Link>
-        <div className={s.title}>
-          <p >{title}</p>
-          {price === 'Бесплатно' ? <p>Цена: {price}</p> : <p>Цена: {price} рублей </p>}
-          
-          </div>
-          <div className={s.overlay} onClick={handleOverlayClick}>
-          
-            <div className={s.like}>
-            <LikeButton id={id}/>
-            </div>
-            <div className={s.cart}>
-            <CartButton />
-            </div>
-          </div>
+      </Link>
+      <div className={s.title}>
+        <p>{title}</p>
+        {price === "Бесплатно" ? (
+          <p>Цена: {price}</p>
+        ) : (
+          <p>Цена: {price} рублей </p>
+        )}
+      </div>
+      <div className={s.overlay}>
+        <LikeButton id={id} size={32} />
+        <div
+          className={isInCart ? s.cart_disabled : s.cart}
+          onClick={handleAddToCart}
+        >
+          <CartButton />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
