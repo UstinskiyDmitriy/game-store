6f05be02a180d5f384ddb202a2d7730eaab0a2f0
UstinskiyDmitriy <ustinskiy.dmitriy1997@yandex.ru>
2024-11-11 19:01:55 +0300

    added component

---
+++ src/components/basket-card/BasketCard.tsx
@@ -0,0 +1,66 @@
+import s from "./BasketCard.module.css";
+import { TCardData } from "../../interfaces/cardInterface";
+import { LucideTrash2 } from "lucide-react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { removeFromCart, incrementQuantity, decrementQuantity } from "../../services/slices/cartSlice";
+import LikeButton from "../../ui/like-button/LikeButton";
+
+export default function BasketCard({ id, image, title, price }: TCardData) {
+  const dispatch = useDispatch();
+  const [count, setCount] = useState(1);
+
+  const handlePlus = () => {
+    if (count < 5) {
+      setCount(count + 1);
+      dispatch(incrementQuantity({ id, price }));
+    }
+  };
+
+  const handleMinus = () => {
+    if (count > 1) {
+      setCount(count - 1);
+      dispatch(decrementQuantity({ id, price }));
+    } else {
+      setCount(count - 1);
+      setTimeout(() => {
+        dispatch(removeFromCart(id));
+      }, 100);
+    }
+  };
+
+  const removeCard = () => {
+    dispatch(removeFromCart(id));
+  };
+
+  return (
+    <div className={s.card} key={id}>
+      <div className={s.card_img_wrapper}>
+        <img src={image} alt="" className={s.card_img} />
+      </div>
+      <div className={s.about_wrapper}>
+        <div className={s.title}>
+          <h3 className={s.card_title}>{title}</h3>
+          <div className={s.buttons}>
+            <LikeButton id={id} size={35}/>
+            <LucideTrash2 size={50} className={s.trash} onClick={removeCard} />
+          </div>
+        </div>
+        <div className={s.price}>
+          <div className={s.counter}>
+            <div onClick={handleMinus} className={s.minus}>
+              <p>-</p>
+            </div>
+            <div className={s.display}>
+              <p>{count}</p>
+            </div>
+            <div onClick={handlePlus} className={s.plus}>
+              <p>+</p>
+            </div>
+          </div>
+          {price === "Бесплатно" ? <p>{price}</p> : <p>{price} рублей</p>}
+        </div>
+      </div>
+    </div>
+  );
+}
