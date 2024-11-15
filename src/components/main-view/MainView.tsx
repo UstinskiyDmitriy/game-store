68c833776ce62f048993fe57906ee9012050e8ce
UstinskiyDmitriy <ustinskiy.dmitriy1997@yandex.ru>
2024-11-15 20:44:14 +0300

    changed sliding method of main view images

--- src/components/main-view/MainView.tsx
+++ src/components/main-view/MainView.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState} from 'react';
 import s from "./MainView.module.css";
 import BackgroundImage from "../../ui/background-image/BackgroundImage";
 import GameInfo from "../../ui/game-info/GameInfo";
@@ -9,20 +9,19 @@ export default function MainView() {
   const cards = useSelector((state: RootState) => state.cards.cards);
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % cards.length); 
-    }, 3000); 
-
-    return () => clearInterval(interval); 
-  }, [cards.length]);
-
   const currentCard = cards[currentIndex];
+
+  const nextCard = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % cards.length);
+  }
+
+  const prevCard = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1) % cards.length);
+  }
 
   return (
     <div className={s.main}>
-      
-      <BackgroundImage image={currentCard.image} alt={currentCard.title} />
+      <BackgroundImage image={currentCard.image} alt={currentCard.title} id={currentCard.id} nextCard={nextCard} prevCard={prevCard}/>
       <div className={s.wrapper}>
         <div></div>
         <div className={s.view_description}>
--- src/ui/background-image/BackgroundImage.tsx
+++ src/ui/background-image/BackgroundImage.tsx
@@ -1,14 +1,27 @@
+import { Link } from "react-router-dom";
 import s from "./BackgroundImage.module.css";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+
 interface BackgroundImageProps {
   image?: string;
   alt?: string;
+  id: number;
+  nextCard: ()=> void;
+  prevCard: ()=> void;
 }
 
-export default function BackgroundImage({ image, alt }: BackgroundImageProps) {
-
+export default function BackgroundImage({ image, alt, id,nextCard,prevCard }: BackgroundImageProps) {
+ 
   return (
     <div className={s.overlay}>
+      <Link to={`/about/${id}`}>
       <img src={image} alt={alt} className={s.background_img} loading="lazy"/>
+      </Link>
+      <div className={s.arrows}>
+      <div><ChevronLeft  onClick={prevCard} /></div>
+      <div><ChevronRight onClick={nextCard}/></div>
+      </div>
+     
     </div>
   );
 }
