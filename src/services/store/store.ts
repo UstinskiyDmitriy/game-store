5af2c0c7d9e8fd71a4a4e4e41807d6915a3c20ef
UstinskiyDmitriy <ustinskiy.dmitriy1997@yandex.ru>
2024-11-26 18:17:00 +0300

    renaming

--- src/services/store/store.ts
+++ src/services/store/store.ts
@@ -2,12 +2,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cardsReducer from '../slices/gameSlice';
 import cartReducer from '../slices/cartSlice';
-import userReducer from '../slices/userSlice'
+import authReducer from '../slices/authSlice'
 export const store = configureStore({
   reducer: {
     cards: cardsReducer,
     cart: cartReducer,
-    user: userReducer,
+    user: authReducer,
   },
 });
 
